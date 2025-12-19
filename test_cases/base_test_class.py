50ba2fa514bfde837683ecfd85466182d87bad9d
laxmi kant joshi <lkjoshi@Mac.broadband>
2025-12-19 10:32:51 +0000

    refactor a class

--- test_cases/base_test_class.py
+++ test_cases/base_test_class.py
@@ -19,7 +19,7 @@ class Test_Base_test_Class:
     page_title_h3 = "h3"
 
     def launch_page(self, page):
-        headless = os.getenv("HEADLESS", "false").lower() == "true"
+        headless = os.getenv("HEADLESS", "false").lower() == "false"
 
         options = webdriver.ChromeOptions()
         if headless:
